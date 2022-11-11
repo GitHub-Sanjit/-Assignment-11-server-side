5e3fe7cbfec5657f45db13dc7d130233d919d2a8
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-11 17:32:22 +0600

    Load service data and create MyReviews page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,8 +24,15 @@ async function run() {
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services)
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/myReview/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
     });
   } finally {
   }
