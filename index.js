4d87114d024db56eb31c75d96a0f9360c64557ea
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-11 21:00:46 +0600

    Add Services successfull

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("auroPress").collection("services");
+    const reviewCollection = client.db("auroPress").collection("reviews");
+    const orderCollection = client.db("auroPress").collection("services");
 
     app.get("/services", async (req, res) => {
       const query = {};
@@ -33,6 +35,27 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const service = await serviceCollection.findOne(query);
       res.send(service);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    //   review api
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+
+    //   orders api
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
     });
   } finally {
   }
